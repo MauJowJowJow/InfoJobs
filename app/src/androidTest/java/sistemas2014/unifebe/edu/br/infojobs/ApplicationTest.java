c8a3827ab80cc110a95590de5c4864b0f4ab4a84
MauJowJowJow <maumau.g.o@hotmail.com>
2016-07-13 20:12:27 -0300

    Commit inicial

---
+++ app/src/androidTest/java/sistemas2014/unifebe/edu/br/infojobs/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sistemas2014.unifebe.edu.br.infojobs;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
