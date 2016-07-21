0a240f3204fe35edce417f758a3aefa6b7e660a9
MauJowJowJow <maumau.g.o@hotmail.com>
2016-07-21 19:58:48 -0300

    Modelos iniciais

--- app/src/main/java/sistemas2014/unifebe/edu/br/infojobs/InfoJobsApp.java
+++ app/src/main/java/sistemas2014/unifebe/edu/br/infojobs/InfoJobsApp.java
@@ -1,7 +1,10 @@
-package sistemas2014.unifebe.edu.br.infojobs.Model;
+package sistemas2014.unifebe.edu.br.infojobs;
 
 import android.app.Application;
 import android.content.res.Configuration;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 /**
  * Created by mauma on 18/07/2016.
@@ -18,6 +21,9 @@ public class InfoJobsApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
         instance = this;
     }
