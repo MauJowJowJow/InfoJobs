2d3c5528a5bcc2fdf443ea6f27394311a339cee9
MauJowJowJow <maumau.g.o@hotmail.com>
2016-08-07 18:53:25 -0300

    Compartilhar vaga

--- app/src/main/java/sistemas2014/unifebe/edu/br/infojobs/Controller/DetalhesVaga.java
+++ app/src/main/java/sistemas2014/unifebe/edu/br/infojobs/Controller/DetalhesVaga.java
@@ -13,6 +13,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+
+import java.util.Arrays;
+
+import sistemas2014.unifebe.edu.br.infojobs.Helpers.BuscaPermissoesFB;
 import sistemas2014.unifebe.edu.br.infojobs.Model.Cargo;
 import sistemas2014.unifebe.edu.br.infojobs.Model.Empresa;
 import sistemas2014.unifebe.edu.br.infojobs.Model.Endereco;
@@ -63,7 +72,7 @@ public class DetalhesVaga extends AppCompatActivity {
         btnCompartilhar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                compartilharVaga();
             }
         });
 
@@ -89,4 +98,47 @@ public class DetalhesVaga extends AppCompatActivity {
         }
     }
 
+    private void compartilharVaga(){
+        boolean publicarPermitido = false;
+        if(AccessToken.getCurrentAccessToken()==null){
+            Toast.makeText(getApplicationContext(), "Nenhum usuário logado com vínculo ao Facebook!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        BuscaPermissoesFB buscaPermissoesFB = new BuscaPermissoesFB();
+        try {
+            publicarPermitido = buscaPermissoesFB.execute(new String[]{"publish_actions"}).get();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        if(!publicarPermitido) {
+            LoginManager.getInstance().logInWithPublishPermissions(
+                    this,
+                    Arrays.asList("publish_actions"));
+        }
+
+        if(publicarPermitido){
+            Bundle params = new Bundle();
+            params.putString("message", "Procurando emprego? Venha conheçer o InfoJobs!" + System.getProperty("line.separator") +
+                                        "Estamos precisando de um " +
+                                         vaga.getCargo().getNome() + System.getProperty("line.separator") +
+                                        "Com salário médio de " + vaga.getSalario() + System.getProperty("line.separator") +
+                                        "Interessado? Entre em contato pelo e-mail: " + vaga.getEmpresa().getEmail());
+
+            new GraphRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    "/me/feed",
+                    params,
+                    HttpMethod.POST,
+                    new GraphRequest.Callback() {
+                        public void onCompleted(GraphResponse response) {
+                            Toast.makeText(getApplicationContext(), "Publicação realizada!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ).executeAsync();
+        }else
+            Toast.makeText(getApplicationContext(), "Não foi concedida a permissão para este app publicar no facebook, " +
+                    "tente novamente para liberar a publicação!", Toast.LENGTH_LONG).show();
+    }
 }
